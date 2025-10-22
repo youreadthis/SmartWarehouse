7f0e9c80a2360e37a408ead8f741de8fedb8c077
Youreadthis <sgv300507@gmail.com>
2025-10-22 13:56:06 +0300

    "ADD: UrlAcess"

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from SmartWarehouse.app.api import main_router
-
+from SmartWarehouse.configFile import url
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[f"{url}"],
+)
 
 app.include_router(router=main_router)
 
