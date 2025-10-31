1477459c471090c0cc2aae6af269c7808ced5489
Youreadthis <sgv300507@gmail.com>
2025-10-31 19:57:41 +0300

    Update to dev

--- app/models/db_session.py
+++ app/models/db_session.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy_utils import create_database, database_exists
 from configFile import postgresql as settings
 
