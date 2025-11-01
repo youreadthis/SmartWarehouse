54611431c3a0bd609b8a409ccb7fc4893df21a0b
Youreadthis <sgv300507@gmail.com>
2025-11-01 10:15:17 +0300

    Add logil logic

--- app/models/users_methods.py
+++ app/models/users_methods.py
@@ -6,9 +6,16 @@ from sqlalchemy import select
 Session = db_session.get_session()
 
 
-async def check_useres(email):
+async def check_useres(email) -> Users | None:
     async with Session() as session:
         query = select(Users).where(Users.email == email)
+        result = await session.execute(query)
+        result = result.scalar_one_or_none()
+        return result
+
+async def login(email, password) -> Users | None:
+    async with Session() as session:
+        query = select(Users).where(Users.email == email and Users.password_hash == password)
         result = await session.execute(query)
         result = result.scalar_one_or_none()
         return result
